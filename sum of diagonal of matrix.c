60aa49adad130046cdd3c13e102986fd1dce93a5
Aman83033 <92243756+Aman83033@users.noreply.github.com>
2021-12-21 22:33:16 +0530

    Add files via upload

---
+++ sum of diagonal of matrix.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int row,col,sum=0,x[3][3];
+	printf("enter the elements for matrix");
+	printf("\n");
+   for(row=0;row<3;row++)
+   {
+   	for(col=0;col<3;col++)
+   	{ printf("elements in [%d],[%d] = ",row,col);
+   		scanf("%d",&x[row][col]);
+	   }
+   }
+  
+   for(row=0;row<3;row++)
+   {
+   	for(col=0;col<3;col++)
+   	{if(row==col)
+   	
+	  sum+=x[row][col];
+   }
+  
+   }
+    printf("the sum of the diagonal of matrix is = %d",sum);
+   return 0;
+}
