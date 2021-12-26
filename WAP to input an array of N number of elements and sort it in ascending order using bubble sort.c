fb4334134cfc7659581f221780626209d69d8ebb
Aman83033 <92243756+Aman83033@users.noreply.github.com>
2021-12-26 23:49:30 +0530

    Add files via upload

---
+++ WAP to input an array of N number of elements and sort it in ascending order using bubble sort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int size,i;
+	printf("enter size of an array");
+	scanf("%d",&size);
+	int temp,a[size],j;
+	
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<size-1;i++)
+	{
+		for(j=0;j<size-i;j++)
+		{if(a[j]>a[j+1])
+			{
+			temp=a[j];
+			a[j]=a[j+1];
+			a[j+1]=temp;
+		}}
+	}
+	for(i=0;i<size;i++)
+	{
+	printf("%d\t",a[i]);
+	}
+	return 0;
+}
+	
