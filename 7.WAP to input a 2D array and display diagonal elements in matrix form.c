5b689a38616676969e8df45d00ef276cd4e47ed0
Aman83033 <92243756+Aman83033@users.noreply.github.com>
2022-01-02 21:06:24 +0530

    Add files via upload

---
+++ 7.WAP to input a 2D array and display diagonal elements in matrix form.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+	int row,col,sum=0,x[3][3];
+	printf("enter the elements for matrix :\n");
+
+   for(row=0;row<3;row++)
+   {
+   	for(col=0;col<3;col++)
+   	{ printf("elements in [%d],[%d] = ",row,col);
+   		scanf("%d",&x[row][col]);
+	   }
+   }
+  for(row=0;row<3;row++)
+   {
+   	for(col=0;col<3;col++)
+   	{ printf("%d\t",x[row][col]);
+   		
+	   }
+	   printf("\n");
+   }
+  
+  
+    
+    printf("the digonal matrix is :\n");
+    for(row=0;row<3;row++)
+   {
+   	for(col=0;col<3;col++)
+   	{if(row==col)
+   {
+		printf("%d\t",x[row][col]);
+	  sum+=x[row][col];
+	  }
+	  else
+	  printf("\t");
+   }
+  printf("\n");
+   }
+   printf("the sum of the diagonal of matrix is = %d\n",sum);
+   return 0;
+}
