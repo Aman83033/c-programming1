d3c1a2a4b3ffe55bbe6d6a157ffad5e82db1a0ba
Aman83033 <92243756+Aman83033@users.noreply.github.com>
2022-01-03 20:19:18 +0530

    Add files via upload

---
+++ printf upper half of a matrix.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	int m,n,i,j,found=0;
+	printf("Enter the number of rows:\n");
+	scanf("%d",&m);
+	printf("Enter the number of columns:\n");
+	scanf("%d",&n);
+	int x[m][n];
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("Elements [%d],[%d]=",i,j);
+			scanf("%d",&x[i][j]);
+		}
+	}
+	printf("-------------------------------------------\n\n");
+	printf("The matrix you inputed:\n\n");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("%d\t",x[i][j]);
+		}
+		printf("\n");
+	}
+	printf("upper half of the matrix isc :\n");
+	for(i=0;i<m;i++)
+	{	
+		for(j=i;j<n;j++)
+		{
+		  printf("%d\t",x[i][j]);
+		   }
+		   printf("\t\n");
+	}
+	return 0;
+}
